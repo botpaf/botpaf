caf4f6b2f4127e7c40f43a2bf9010db3a74f70a5
botpaf <beepaf@gmail.com>
2018-10-20 22:41:40 +0200

    🛠 add channels to ini file

--- src/Bot/IRC/Config.hs
+++ src/Bot/IRC/Config.hs
@@ -15,10 +15,11 @@ import Lens.Micro.TH (makeLenses)
 import Data.Ini.Config.Bidir
 
 data Config = Config
-  { _host :: Text
-  , _port :: Int
-  , _name :: Text
-  , _token :: Text
+  { _host     :: Text
+  , _port     :: Int
+  , _name     :: Text
+  , _token    :: Text
+  , _channels :: [Text]
   } deriving (Eq, Show)
 
 makeLenses ''Config
@@ -28,6 +29,7 @@ configSpec = do
   section "NETWORK" $ do
     host .= field "host" text
     port .= field "port" number
+    channels .= field "channels" (listWithSeparator "," text)
   section "AUTH" $ do
     name .= field "name" text
     token .= field "token" text
@@ -38,11 +40,14 @@ defaultConfig = Config
   , _port = 0
   , _name = "default"
   , _token = "default"
+  , _channels = []
   }
 
 -- TODO maybe don't throw exceptions this brutally
 parseConfig :: FilePath -> IO Config
 parseConfig filename = do
-  Right config <- parseIni <$> Text.readFile filename
-                           <*> pure (ini defaultConfig configSpec)
-  pure $ getIniValue config
+  res <- parseIni <$> Text.readFile filename
+                  <*> pure (ini defaultConfig configSpec)
+  case res of
+    Left err -> error err
+    Right config -> pure $ getIniValue config
