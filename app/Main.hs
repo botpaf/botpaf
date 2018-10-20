bcd69df9ea4429342bfa100702d498ebdf536d6c
botpaf <beepaf@gmail.com>
2018-10-20 20:40:30 +0200

    🛠 first connecting binary 🌟

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,11 @@ import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
 import Bot.IRC.Config
+import Bot.IRC
 
 main :: IO ()
 main = do
   config <- parseConfig "botpaf.ini"
-  print config
+  withConnection config $ \h -> do
+    sendHello config h
+    eventLoop config h
