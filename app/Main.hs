cd97e2801aa1299d8fb09a600132c6b4b9245d72
botpaf <beepaf@gmail.com>
2018-10-21 22:12:20 +0200

    🛠 add a risky command

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+-- import Data.Foldable ( for_ )
+
 -- import Data.Text (Text)
--- import qualified Data.Text as Text
+import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+
+-- import Control.Monad ( when )
 
 import Control.Concurrent.Async ( Async, async )
 import Control.Concurrent.STM ( atomically, TQueue, newTQueueIO, readTQueue )
@@ -34,10 +38,24 @@ getEvent = atomically . readTQueue . ircMessages
 main' :: Bot -> IO ()
 main' bot = do
   ev <- getEvent bot
-  case ev of
-    _ -> doEchoRawMsg bot ev >>= main'
+  case ev ^. msgCommand of
+    "PRIVMSG" -> resume =<< doMsg bot ev
+    _         -> main' =<< doEchoRawMsg bot ev
+  where
+    resume Nothing     = pure ()
+    resume (Just bot') = main' bot'
 
 doEchoRawMsg :: Bot -> RawIrcMsg -> IO Bot
 doEchoRawMsg bot msg = do
   Text.putStr $ "BOT: " <> renderMsg msg
   return bot
+
+doMsg :: Bot -> RawIrcMsg -> IO (Maybe Bot)
+doMsg bot msg = do
+  let [room,text] = msg ^. msgParams
+  if ("!quit" `Text.isPrefixOf` text)
+  then do
+    Text.putStrLn $ "received quit signal in " <> room
+    return Nothing
+  else
+    return $ Just bot
