b25f7d7b1c27276d72591925cf5b49143aab5ab0
botpaf <botpaf@example.com>
2018-10-21 15:29:32 +0200

    🛠 Bot.IRC.Config export lenses
    Closes #1

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,13 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
-import Bot.IRC.Config
 import Bot.IRC
+import Bot.IRC.Config
 
 main :: IO ()
 main = do
   config <- parseConfig "botpaf.ini"
   withConnection config $ \h -> do
     sendHello config h
-    sendJoin (_channels config) h
+    sendJoin (config ^. channels) h
     eventLoop config h
