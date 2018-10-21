3f6322781f8c86d03a3419538ddf05897a737358
botpaf <beepaf@gmail.com>
2018-10-21 19:56:50 +0200

    🛠 delete yesterday’s attempt

--- src/Bot/IRC/Msg.hs
+++
@@ -1,9 +0,0 @@
-module Bot.IRC.Msg where
-
-import Irc.RawIrcMsg ( TagEntry )
-import Irc.Message   ( IrcMsg )
-
-data Msg = Msg
-  { _tags :: [TagEntry] -- TODO: this needs to be a strict ADT not a lazy list
-  , _msg :: !IrcMsg
-  } deriving (Show)
